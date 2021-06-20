ce6a32b3da0dcb0ee3ae640509e5f71c953be329
Shady <shady.ber9@gmail.com>
2021-06-20 18:48:44 +0300

    route
    route

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/userprofile', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\HomeController::class, 'index'])->name('profile');
+Route::get('/userprofile', [App\Http\Controllers\HomeController::class, 'index'])->name('userprofile');
 
 Route::resource('/blog', App\Http\Controllers\BlogController::class);
 Route::resource('/category', App\Http\Controllers\BlogCategoryController::class);
